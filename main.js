19995bf625c1ac3966b0ff01cdc199ea54fd7804
unknown <matviienko.i.o@gmail.com>
2018-04-02 21:27:03 +0300

    init

--- main.js
+++ main.js
@@ -33,17 +33,43 @@ $(function(){
 		  "experience":1
 	   }
 	};
-	var arr = [];
+	
+    //1) object to arr
+    let arr = [];
 	$.each(dataFromAndrey, function(key, value){
 		value.name = key;
 		arr.push(value);
 	})
 	console.log(arr);
 	
-	
+	//2) sort by dateworking time
 	console.log(
 		arr.sort(function(a, b){
 			return (a.age - a.experience) - (b.age - b.experience);
 		})
 	);
-});
+    
+    //3) sort experience > 6 years
+    let arrMoreSixExp = arr.filter(function(value){
+        return value.experience > 6;
+    });
+    console.log(arrMoreSixExp);
+    
+    let arrThirsty = [],
+        arrThirstyFourty = [],
+        arrFourty = [];
+    $.each(arr, function(key, items){        
+        if(items.age <= 30){            
+            arrThirsty.push(items);
+        }
+        else if(30 < items.age && items.age < 40){
+            arrThirstyFourty.push(items);            
+        }
+        else{
+            arrFourty.push(items);            
+        }             
+    });    
+    console.log(arrThirsty);
+    console.log(arrThirstyFourty);
+    console.log(arrFourty);
+})
