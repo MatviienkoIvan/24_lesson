dc4d2d42574b30197b05d7b3d4a61cb57651c458
Anna <matviienko.i.o@gmail.com>
2018-04-03 21:59:13 +0300

    classworks

--- main.js
+++ main.js
@@ -34,21 +34,35 @@ $(function(){
 	   }
 	};	
     //1) object to arr
-    let arr = [];
+    /*
+	let arr = [];
 	$.each(dataFromAndrey, function(key, value){
 		value.name = key;
 		arr.push(value);
 	})
-	console.log(arr);	
+	console.log(arr);
+	*/
+	
+    let arr1 = [];
+	$.each(dataFromAndrey, function(key, value){
+		let obj = {};
+			obj.name = key;
+			obj.age = value.age;		
+			obj.experience = value.experience;
+			arr1.push(obj);
+	})
+	console.log(arr1);
+	
+	
 	//2) sort by dateworking time
 	console.log(
-		arr.sort(function(a, b){
+		arr1.sort(function(a, b){
 			return (a.age - a.experience) - (b.age - b.experience);
 		})
 	);
     
     //3) sort experience > 6 years
-    let arrMoreSixExp = arr.filter(function(value){
+    let arrMoreSixExp = arr1.filter(function(value){
         return value.experience > 6;
     });
     console.log(arrMoreSixExp);
@@ -56,7 +70,7 @@ $(function(){
     let arrThirsty = [],
         arrThirstyFourty = [],
         arrFourty = [];
-    $.each(arr, function(key, items){        
+    $.each(arr1, function(key, items){        
         if(items.age <= 30){            
             arrThirsty.push(items);
         }
@@ -71,23 +85,66 @@ $(function(){
     console.log(arrThirstyFourty);
     console.log(arrFourty);
 	
-	let alfabet =["A","B","C","D","E","F",
-				"G","H","I","J","K","L","M",
-				"N","O","P","Q","R","S","T",
-				"U","V","W","X","Y","Z","1",
-				"2","3","4","5","6","7","8",
-				"9","0"," "],
-		morzeAlfabet = [".-","-...","-.-.","-..",
-				".","..-.","--.","....","..",
-				".---","-.-",".-..","--","-.",
-				"---",".--.","--.-",".-.","...",
-				"-","..-","...-",".--","-..-",
-				"-.--","-..",".----","..---", 
-				"...--","....-",".....","-....",
-				"--...","---..","----.","-----", 
-				"......."];
-				
-	function translateAlfabetToMorze(param){
-		
+	var obj = {
+		"A": ".-",
+		"B": "-...",
+		"C": "-.-.",
+		"D": "-..",
+		"E": ".",
+		"F": "..-.",
+		"G": "--.",
+		"H": "....",
+		"I": "..",
+		"J": ".---",
+		"K": "-.-",
+		"L": ".-..",
+		"M": "--",
+		"N": "-.",
+		"O": "---",
+		"P": ".--.",
+		"Q": "--.-",
+		"R": ".-.",
+		"S": "...",
+		"T": "-",
+		"U": "..-",
+		"V": "...-",
+		"W": ".--",
+		"X": "-..-",
+		"Y": "-.--",
+		"Z": "--.."
 	}
+				
+	function translateAlfabetToMorze(value){
+		let arrMorz = value.split(""),
+			separateChar = " ",
+			separateWord = "   ",
+			result;
+		arrMorz.forEach(function(item){
+			return result += obj[item.toUpperCase()] + separateChar;
+		})
+		return "Morze text" + result;
+	}	
+	translateAlfabetToMorze("Hello world")
+	
+	var coins = {
+		penny: 0.01,
+		nickel: 0.05,
+		dime: 0.10,
+		quarter: 0.25,
+		dollar: 1.00
+	}
+	function change_count(param){
+		let arrParam = [],
+			total = 0;
+		arrParam = param.split(" ");
+		arrParam.forEach(function(item){
+			return total += coins[item];
+		})
+		return '$' + total;
+	}	
+	change_count("nickel penny dime dollar")
+	
 })
+
+// тороны джаваскрипт
+// https://www.codewars.com/
